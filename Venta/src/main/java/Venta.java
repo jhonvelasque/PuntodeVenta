36e7ccb29b8d75cba0af5ab54378c9d73a841441
jhonvelasque <jhon.velasque@unmsm.edu.pe>
2023-08-17 18:21:12 -0500

    avance

---
+++ Venta/src/main/java/Venta.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author carlo
+ */
+public class Venta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
