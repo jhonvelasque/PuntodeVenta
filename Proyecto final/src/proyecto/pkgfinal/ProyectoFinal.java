8e7c7a9169870dfa4c96d6243752fb63c4337291
jhonvelasque <jhon.velasque@unmsm.edu.pe>
2023-08-19 09:59:16 -0500

    modifique el proyecto

--- Proyecto final/src/proyecto/pkgfinal/ProyectoFinal.java
+++ Proyecto final/src/proyecto/pkgfinal/ProyectoFinal.java
@@ -12,6 +12,8 @@ public class ProyectoFinal {
      */
     public static void main(String[] args) {
         System.out.println("hola mundo alvaro");
+        System.out.println("jhon estuvo aqui tambien ");
+        
     }
     
 }
