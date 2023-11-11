d30345ce59255ba934e4b9ec58c4c9c32240102e
jhonvelasque <jhon.velasque@unmsm.edu.pe>
2023-11-11 01:17:24 -0500

    avanze de tabla categoria producto

---
+++ Proyecto final/src/BEAN/Categoria_producto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BEAN;
+
+/**
+ *
+ * @author carlo
+ */
+public class Categoria_producto {
+    private int id_categoria ;
+    
+}
