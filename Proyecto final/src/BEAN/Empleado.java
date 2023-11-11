d6f3fbc0e96409da5ac6aad5f621254c429984c4
VICTORAGUIRRESAENZ <victoronan.aguirre@unmsm.edu.pe>
2023-11-11 00:41:46 -0500

    victor es cagon

---
+++ Proyecto final/src/BEAN/Empleado.java
@@ -0,0 +1,16 @@
+
+package BEAN;
+
+public class Empleado {
+    
+private int idEmpleado;
+private String NomEmpleado;
+private String ApellEmpleado;
+private String DocEmpleado;
+private String Direccion;
+private String TelfEmpleado;
+private String Correo;
+private int estado;
+//private String ;
+    
+}
