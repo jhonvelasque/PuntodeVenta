a7647211aee60b2102e3ce18e58b3073534efa0e
VICTORAGUIRRESAENZ <victoronan.aguirre@unmsm.edu.pe>
2023-11-11 01:35:26 -0500

    actualiza, rol

--- Proyecto final/src/BEAN/Rol.java
+++ Proyecto final/src/BEAN/Rol.java
@@ -5,4 +5,38 @@ public class Rol {
     private int idRol;
     private String DesRol;
     private double Mensualidad;
+
+    public Rol() {
+    }
+
+    public Rol(int idRol, String DesRol, double Mensualidad) {
+        this.idRol = idRol;
+        this.DesRol = DesRol;
+        this.Mensualidad = Mensualidad;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getDesRol() {
+        return DesRol;
+    }
+
+    public void setDesRol(String DesRol) {
+        this.DesRol = DesRol;
+    }
+
+    public double getMensualidad() {
+        return Mensualidad;
+    }
+
+    public void setMensualidad(double Mensualidad) {
+        this.Mensualidad = Mensualidad;
+    }
+    
 }
