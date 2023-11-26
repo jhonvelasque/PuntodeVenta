f0ba191c8185206c2446fed97960dc7e51e3c64a
AlvaroC1234 <142675010+AlvaroC1234@users.noreply.github.com>
2023-11-26 11:58:09 -0500

    Update Producto.java

--- Proyecto final/src/BEAN/Producto.java
+++ Proyecto final/src/BEAN/Producto.java
@@ -2,6 +2,7 @@ package BEAN;
 public class Producto {
     private int IdProducto;
     private int IdCategoria;
+    private String Marca;
     private String NombreProducto;
     private double  CostoUnitario;
     private double PrecioVenta;
@@ -11,9 +12,10 @@ public class Producto {
     public Producto(){
     }
     
-    public Producto (int IdProducto, int IdCategoria, String NombreProducto, double CostoUnitario, double PrecioVenta, String FechaIngreso, int Estado){
+    public Producto (int IdProducto, int IdCategoria, String Marca, String NombreProducto, double CostoUnitario, double PrecioVenta, String FechaIngreso, int Estado){
         this.IdProducto = IdProducto;
         this.IdCategoria = IdCategoria;
+        this.Marca = Marca;
         this.NombreProducto = NombreProducto;
         this.CostoUnitario = CostoUnitario;
         this.PrecioVenta = PrecioVenta;
@@ -35,6 +37,14 @@ public class Producto {
 
     public void setIdCategoria(int IdCategoria) {
         this.IdCategoria = IdCategoria;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
     }
 
     public String getNombreProducto() {
@@ -76,6 +86,5 @@ public class Producto {
     public void setEstado(int Estado) {
         this.Estado = Estado;
     }
-    
     
 }
